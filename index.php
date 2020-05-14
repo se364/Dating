f875f3b9beda0a1d7a4b4b162e48cc50734a0bed
Shah Emran <semran@mail.greenriver.edu>
2020-05-14 00:36:30 -0400

    Update the index with route to three pages

--- index.php
+++ index.php
@@ -7,9 +7,13 @@
  * Dating Website
  */
 
-//Turn on error reporting -- this is critical!
+//Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+//Start a session
+session_start();
+
 
 //Require the autoload file
 require_once("vendor/autoload.php");
@@ -21,9 +25,95 @@ $f3 = Base::instance();
 $f3->route('GET /', function(){
     //echo '<h1> Pet Home</h1>';
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/pet-home.html');
 }
 );
+
+
+//Order route
+$f3->route('GET|POST /person', function($f3) {
+
+    $meals = getMeals();
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
+        //["food"]=>"tacos" ["meal"]=>"lunch"
+
+        //Validate the data
+        if (empty($_POST['food']) || !in_array($_POST['meal'], $meals)) {
+            echo "<p>Please enter a food and select a meal</p>";
+        }
+        //Data is valid
+        else {
+            //Store the data in the session array
+            $_SESSION['food'] = $_POST['food'];
+            $_SESSION['meal'] = $_POST['meal'];
+
+            //Redirect to Order 2 page
+            $f3->reroute('order2');
+        }
+    }
+
+    $f3->set('meals', $meals);
+    $view = new Template();
+    echo $view->render('views/PersonInfo.html');
+
+});
+
+//Order route
+$f3->route('GET|POST /profile', function($f3) {
+
+    $conds = getCondiments();
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //Store the data in the session array
+        $_SESSION['conds'] = $_POST['conds'];
+
+        //Redirect to summary page
+        $f3->reroute('summary');
+    }
+
+    $f3->set('conds', $conds);
+    $view = new Template();
+    echo $view->render('views/ProfileInfo.html');
+});
+
+//Breakfast route
+
+$f3->route('GET|POST /interest', function($f3) {
+
+    $conds = getCondiments();
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //Store the data in the session array
+        $_SESSION['conds'] = $_POST['conds'];
+
+        //Redirect to summary page
+        $f3->reroute('summary');
+    }
+
+    $f3->set('conds', $conds);
+    $view = new Template();
+    echo $view->render('views/Interests.html');
+});
+
+
+
+
+// Summary of the profile
+$f3->route('GET /summary', function() {
+    //echo '<h1>Thank you for your order!</h1>';
+
+    $view = new Template();
+    echo $view->render('views/ProfileSummary.html');
+
+    session_destroy();
+});
 
 // Run fat free
 $f3->run();
